d0fa758a8ec40141c339dde3f7efb9b2e780f535
Atabey Ünlü <atabeyunlu36@gmail.com>
2023-02-17 11:31:35 +0300

    Update dataset_download.sh

--- data/dataset_download.sh
+++ data/dataset_download.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 pip install gdown
 
-gdown --fuzzy "https://drive.google.com/file/d/1GPvLp14n57kTgyYV-UUn4kJTNt2vvPQs/view?usp=sharing"
+gdown --fuzzy "https://drive.google.com/file/d/1kDpTm36X3ugpr6Ooo4Fg_dkNRZhQ5EMC/view?usp=share_link"
+
+gdown --fuzzy "https://drive.google.com/file/d/13h465yaIbrAp5tcGbIwhxriorejr6Fsz/view?usp=share_link"
