81431bf1a246bb6cb1f457a312f90c9133d90bd9
osbm <osmanfbayram@gmail.com>
2023-09-11 23:23:10 +0300

    add functions

--- druggen/utils.py
+++ druggen/utils.py
@@ -20,6 +20,31 @@ import warnings
 from multiprocessing import Pool
 
 
+def gradient_penalty(y, x, device):
+    """Compute gradient penalty: (L2_norm(dy/dx) - 1)**2."""
+    
+    weight = torch.ones(y.size(),requires_grad=False).to(device)
+    dydx = torch.autograd.grad(outputs=y,
+                                inputs=x,
+                                grad_outputs=weight,
+                                retain_graph=True,
+                                create_graph=True,
+                                only_inputs=True)[0]
+
+    dydx = dydx.view(dydx.size(0), -1)
+    gradient_penalty = ((dydx.norm(2, dim=1) - 1) ** 2).mean()
+    
+    return gradient_penalty
+
+def print_network(model, name):
+    """Print out the network information."""
+    num_params = 0
+    for p in model.parameters():
+        num_params += p.numel() 
+    print(model)
+    print(name)
+    print("The number of parameters: {}".format(num_params))
+
 def matrices2mol(node_labels=None, edge_labels=None, atom_decoders=None, bond_decoders=None, strict=True, file_name=None):
     mol = Chem.RWMol()
     RDLogger.DisableLog('rdApp.*') 
